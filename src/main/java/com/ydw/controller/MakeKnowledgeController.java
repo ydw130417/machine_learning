00d7444f71d73e9753af6913ea74383591af738e
ydwydw <307088683@qq.com>
2018-01-14 20:35:56 +0800

    实现文件的上传和下载 实现图片的文字识别

--- src/main/java/com/ydw/controller/MakeKnowledgeController.java
+++ src/main/java/com/ydw/controller/MakeKnowledgeController.java
@@ -1,7 +1,8 @@
 package com.ydw.controller;
 
-import com.ydw.config.upload.StorageService;
+import com.ydw.model.es_model.TimuDocument;
 import com.ydw.model.jpa_model.Base_timu_search;
+import com.ydw.repository.es_repository.TimuDocumentRepository;
 import com.ydw.repository.jap_repository.BaseTimuSearchRepository;
 import com.ydw.utils.baidu.BaituUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
-import java.util.List;
+import java.io.IOException;
+import java.util.Optional;
 
 /**
  * Created by ydw on 2018/1/12.
@@ -28,6 +29,9 @@ public class MakeKnowledgeController {
     @Autowired
     BaituUtils baituUtils;
 
+    @Autowired
+    TimuDocumentRepository timuDocumentRepository;
+
 
     @PostMapping("/findTimu")
     public @ResponseBody String findTimu(String words){
@@ -39,10 +43,28 @@ public class MakeKnowledgeController {
 
 
     @PostMapping("/upload")
-    public @ResponseBody String upLoadFiles(MultipartFile first){
-        System.out.println(first.getOriginalFilename());
-//        System.out.println("...");
-//        System.out.println(first.getOriginalFilename());
+    public @ResponseBody String upLoadFiles(MultipartFile first, HttpServletRequest request,String timuId){
+        String realPath = request.getSession().getServletContext().getRealPath("/");
+        System.out.println(realPath);
+        String templePath="/Users/mac/Pictures/uplad/";
+        String filename = first.getOriginalFilename();
+        try {
+            File templeFile = new File(templePath + filename);
+            first.transferTo(templeFile);
+            String contentByImgPath = baituUtils.getContentByImgPath(templeFile.getAbsolutePath());
+            Optional<TimuDocument> byId = timuDocumentRepository.findById(timuId);
+            if (byId.isPresent()) {
+                //如果题目存在则更新第一个搜索内容
+                byId.get().setFirstContent(contentByImgPath);
+                timuDocumentRepository.save(byId.get());
+            }else {
+                //如果这个文档不存在,先去数据库查询所有的信息,完善Document然后执行保存
+                System.out.println("完善题目信息,然后进行保存索引");
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return  "upload";
     }
 
